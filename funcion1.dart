47bf75314c0543f4212a1d02a92dff548ab3f47e
David-Salazar-1082 <164534046+David-Salazar-1082@users.noreply.github.com>
2025-02-07 16:47:43 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,20 @@
+// funcion que no devuelve valor
+// funcion saludar
+void saludar(){
+  print('Hola mundo');
+}
+
+void escribeNombre(){
+  print('David Munoz');
+}
+
+void main(){
+  // Kristian David Munoz Salazar Mat: 22308051281082
+  // Llamar a la funcion saludar
+ print('Kristian David Munoz Salazar Mat: 22308051281082');
+ print('Llamando a la funcion saludar');
+  saludar();
+ print('Llamando a la funcion escribeNombre');
+  escribeNombre();
+
+}
